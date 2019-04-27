035e3b062d458d2a81df9604d029a4b9af622ba6
TomyJPN <masaya120331@gmail.com>
2019-04-27 22:04:08 +0900

    AtCoder ABC125

---
+++ AtCoder/ABC/125/A.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int A,B,T,ans;
+
+	scanf("%d %d %d",&A,&B,&T);
+
+	ans=(T/A)*B;
+
+	cout<<ans;
+	
+	return 0;	
+
+}
---
+++ AtCoder/ABC/125/C.cpp
@@ -0,0 +1,26 @@
+//むり
+
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a,int b){
+	if(b==0) return a;
+	return gcd(b,a%b);
+}
+
+
+int main(){
+	int a,b;
+
+	scanf("%d %d",&a,&b);
+
+	cout<<gcd(a,b);
+	
+
+
+	return 0;	
+
+}
+
