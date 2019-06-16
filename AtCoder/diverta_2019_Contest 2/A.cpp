b6cc79d382181f61a8f5786ba3e0b724b29e8d8a
TomyJPN <masaya120331@gmail.com>
2019-06-16 22:51:12 +0900

    AtCoder diverta_2019_Contest

---
+++ AtCoder/diverta_2019_Contest 2/A.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+int main(){
+	int n,k;
+	scanf("%d %d",&n,&k);
+	printf("%d",n%k);
+
+	return 0;
+}
